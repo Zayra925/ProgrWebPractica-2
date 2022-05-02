cafb3fbffb769af57f7bb9f1e80868deee516459
SirGball <103397787+SirGball@users.noreply.github.com>
2022-05-02 12:01:49 -0500

    Practica2ProgWeb
    Repositorio de Practica 2
    Enrique Iván García Castañeda

---
+++ html/scripts.js
@@ -0,0 +1,18 @@
+let products =[];
+let total = 0;
+
+function add(product, price){
+    console.log(product, price);
+    products.push(product);
+    total=total + price;
+    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+}
+
+function pay(){
+    window.alert(products.join(",\n"));
+}
+
+function red(){
+    window.location.href = "log.html";
+
+}
